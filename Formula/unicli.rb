4b49343e7abe560873a20c0b1236db3963e0a58b
Yuichiro MUKAI <y.iky917@gmail.com>
2026-02-21 21:34:43 +0900

    Update unicli to 0.11.1

--- Formula/unicli.rb
+++ Formula/unicli.rb
@@ -1,16 +1,16 @@
 class Unicli < Formula
   desc "CLI tool to control Unity Editor from the terminal"
   homepage "https://github.com/yucchiy/UniCli"
-  version "0.11.0"
+  version "0.11.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/yucchiy/UniCli/releases/download/v#{version}/unicli-osx-arm64.tar.gz"
-      sha256 "ce3b1332e8fe57bbfa6f5e331e1665917d3b1022366522a6d74fbacecb4691ad"
+      sha256 "12b17f8f6ba21ba6ef66b4dfeea42c0b9da6905189d05f69e9dd0aab061f8478"
     else
       url "https://github.com/yucchiy/UniCli/releases/download/v#{version}/unicli-osx-x64.tar.gz"
-      sha256 "4796d42296f9f8bb432d5bc0c392917f84f1447d762cdeb1b47b77f0dd35a975"
+      sha256 "3403e40ae9ae2a1f72878944bfb351d8c3afc8064780bc7d38b363d6e90de782"
     end
   end
 
